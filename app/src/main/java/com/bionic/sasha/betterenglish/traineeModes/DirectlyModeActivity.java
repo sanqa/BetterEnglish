af82d2ed65e49c3583fb8cfe4d096269ecc796eb
1nDisguise <alexander.gordijenko@gmail.com>
2016-12-10 16:48:58 +0200

    changed layout for mode 3

--- app/src/main/java/com/bionic/sasha/betterenglish/traineeModes/DirectlyModeActivity.java
+++ app/src/main/java/com/bionic/sasha/betterenglish/traineeModes/DirectlyModeActivity.java
@@ -11,5 +11,6 @@ public class DirectlyModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directly_mode);
+
     }
 }
