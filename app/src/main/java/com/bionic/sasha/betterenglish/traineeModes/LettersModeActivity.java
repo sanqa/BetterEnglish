8e0f6925e73c2dcd46ede99140897fd87556d343
1nDisguise <alexander.gordijenko@gmail.com>
2016-12-10 15:37:16 +0200

    3,4 modes activities

---
+++ app/src/main/java/com/bionic/sasha/betterenglish/traineeModes/LettersModeActivity.java
@@ -0,0 +1,15 @@
+package com.bionic.sasha.betterenglish.traineeModes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bionic.sasha.betterenglish.R;
+
+public class LettersModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_letters_mode);
+    }
+}
