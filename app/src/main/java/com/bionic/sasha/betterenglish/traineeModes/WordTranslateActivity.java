fdb35b70526e9ed4d2c4cd1b55a565e186ed541a
SASHA <qa.san@yandex.ru>
2017-01-04 14:12:45 +0200

    Craete dialogs which show correct translates in 1 mode

--- app/src/main/java/com/bionic/sasha/betterenglish/traineeModes/WordTranslateActivity.java
+++ app/src/main/java/com/bionic/sasha/betterenglish/traineeModes/WordTranslateActivity.java
@@ -43,6 +43,7 @@ public class WordTranslateActivity extends AppCompatActivity {
     private int correctAnswers = 0;
     public int currentCount = 1;
     public String answer = "";
+    public String word = "";
 
 
     @BindView(R.id.trainee_card_layout)
@@ -92,16 +93,15 @@ public class WordTranslateActivity extends AppCompatActivity {
         allWords.setText("" + allCount);
 
 
-
-
         answer = workingWithDB();
     }
 
     /**
      * Метод для необходимой работы с БД
+     *
      * @return ответ, который должен ввести пользователь.
      */
-    public String workingWithDB(){
+    public String workingWithDB() {
         Random random = new Random(); //создаем экземпляр рандома для получения рандомной строки таблицы
         ArrayList<String> variants = new ArrayList<>(); //сюда передадим возможные варианты ответов на кнопках. Всего будет 4.
         String answer = ""; //строка, с которой будем сравнивать
@@ -111,10 +111,10 @@ public class WordTranslateActivity extends AppCompatActivity {
         int position; //текущяя позиция курсора
 
         SQLiteDatabase database = dbHelper.getWritableDatabase(); //Создаем экземпляр БД и соединяем с нашим помощником.
-        Cursor c = database.query(TranslateReaderDB.TranslateTexts.TABLE_NEW_WORD_NAME,null, selection1, null, null, null, null);
+        Cursor c = database.query(TranslateReaderDB.TranslateTexts.TABLE_NEW_WORD_NAME, null, selection1, null, null, null, null);
         //Это был прямой запрос к таблице New Word Name с некоторым требованием.
-        if (c != null){
-            if (c.moveToFirst()){
+        if (c != null) {
+            if (c.moveToFirst()) {
                 do {
                     count++; //считаем количество удовлетв. строк табицы
                 } while (c.moveToNext());
@@ -122,15 +122,15 @@ public class WordTranslateActivity extends AppCompatActivity {
 
             c.moveToFirst(); // переходим к первой строке полученной таблицы
 
-            do { // так я вывожу свою БД в лог для контроля происходящего. Во время оптимизации кода удалю)
+            do
+            { // так я вывожу свою БД в лог для контроля происходящего. Во время оптимизации кода удалю)
                 Log.d("my DB", "id = " + c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts._ID))
                         + " , ru = " + c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_WORD_RU))
                         + " , en = " + c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_WORD_EN))
                         + " , mode1 = " + c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_MODE1)));
-            } while (c.moveToNext()); //выовдится построчно, пока есть возможность перейти к следующей строке
+            }
+            while (c.moveToNext()); //выовдится построчно, пока есть возможность перейти к следующей строке
         }
-
-
 
 
         // Дальше, если есть записи в таблице, переходим
@@ -140,6 +140,7 @@ public class WordTranslateActivity extends AppCompatActivity {
             c.moveToPosition(position); //переходим к этой строке в таблице
             used.add(position); //добавляем в использованные
             Log.d("used", String.valueOf(position));
+            word = c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_WORD_EN));
             wordTrainee.setText(c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_WORD_EN))); //записываем в карточку
             answer = c.getString(c.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_WORD_RU));
             // записываем ожидаемый ответ пользователя
@@ -150,7 +151,7 @@ public class WordTranslateActivity extends AppCompatActivity {
             for (int i = 0; i < 3; i++) { //тут генерируем еще 3 позиции чтобы не повторялись и добавляем в Лист возможных ответов
                 do {
                     position = random.nextInt(count);
-                }  while (used.contains(position));
+                } while (used.contains(position));
                 used.add(position);
                 Log.d("used", String.valueOf(position));
 
@@ -170,6 +171,7 @@ public class WordTranslateActivity extends AppCompatActivity {
 
     /**
      * Метод отклика на кнопочке
+     *
      * @param view
      */
     public void onClick(View view) {
@@ -177,60 +179,129 @@ public class WordTranslateActivity extends AppCompatActivity {
         Button b = (Button) view;
         String correct = (String) b.getText(); //считываю значение с нажатой кнопки
 
-        if (currentCount < allCount){ //если текущее значение меньше общего проверяю
+        if (currentCount < allCount) { //если текущее значение меньше общего проверяю
 
-            if (correct.equals(answer)){ //если пользователь нажал правильно
-              //  Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            if (correct.equals(answer)) { //если пользователь нажал правильно
+                //  Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
 
                 correctAnswers++;
-                changeModeCorrectResult(answer); //запускаю метод работы с БД для правильного ответа
                 right.setVisibility(View.VISIBLE);
                 wrong.setVisibility(View.INVISIBLE);
+
+
+                changeModeCorrectResult(answer); //запускаю метод работы с БД для правильного ответа
+
+
+                answer = workingWithDB(); //после произведения изменений в БД обновляю текущее слово и ответ
+                currentCount++; //изменяю счетчик текущего слова
+                traineeWords.setText("" + currentCount); //записываю слово в карточку для пользователя
+
             } else {
-                changeModeWrongResult(answer); // для неправильнного ответа
 
                 wrong.setVisibility(View.VISIBLE);
                 right.setVisibility(View.INVISIBLE);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("Wrong").setMessage(word + "  -  " + answer).setCancelable(false)
+                        .setIcon(R.drawable.wrong).setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        changeModeWrongResult(answer); // для неправильнного ответа
+
+
+                        answer = workingWithDB(); //после произведения изменений в БД обновляю текущее слово и ответ
+                        currentCount++; //изменяю счетчик текущего слова
+                        traineeWords.setText("" + currentCount); //записываю слово в карточку для пользователя
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
             }
-           answer =  workingWithDB(); //после произведения изменений в БД обновляю текущее слово и ответ
 
-
-            currentCount++; //изменяю счетчик текущего слова
-            traineeWords.setText("" + currentCount); //записываю слово в карточку для пользователя
         } else {
-            if (correct.equals(answer)){
-                correctAnswers++; //после 10 слова не считывало правильный вариант, поэтому добавил такой костыль =)
+
+            if (correct.equals(answer)) { //если пользователь нажал правильно
+
+                correctAnswers++;
+                changeModeCorrectResult(answer); //запускаю метод работы с БД для правильного ответа
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                //запускааю окно с разными параметрами и правильным кол-вом ответов
+                builder.setTitle("Result!")
+                        .setCancelable(false)
+                        .setMessage("You have " + correctAnswers + " correct answers.")
+                        .setNegativeButton("Change Mode", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+
+                                Intent intent = new Intent(WordTranslateActivity.this, OurDictionaryActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setPositiveButton("This Mode", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+
+                                Intent intent = new Intent(WordTranslateActivity.this, WordTranslateActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+
+            } else {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("Wrong").setMessage(word + "  -  " + answer).setCancelable(false)
+                        .setIcon(R.drawable.wrong).setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        changeModeWrongResult(answer); // для неправильнного ответа
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(WordTranslateActivity.this);
+                        //запускааю окно с разными параметрами и правильным кол-вом ответов
+                        builder.setTitle("Result!")
+                                .setCancelable(false)
+                                .setMessage("You have " + correctAnswers + " correct answers.")
+                                .setNegativeButton("Change Mode", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+
+                                        Intent intent = new Intent(WordTranslateActivity.this, OurDictionaryActivity.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setPositiveButton("This Mode", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+
+                                        Intent intent = new Intent(WordTranslateActivity.this, WordTranslateActivity.class);
+                                        startActivity(intent);
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
             }
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            //запускааю окно с разными параметрами и правильным кол-вом ответов
-            builder.setTitle("Result!")
-                    .setCancelable(false)
-                    .setMessage("You have " + correctAnswers + " correct answers.")
-                    .setNegativeButton("Change Mode", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    dialog.cancel();
-
-                                    Intent intent = new Intent(WordTranslateActivity.this, OurDictionaryActivity.class);
-                                    startActivity(intent);
-                                }
-                            })
-                    .setPositiveButton("This Mode", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();
-
-                            Intent intent = new Intent(WordTranslateActivity.this, WordTranslateActivity.class);
-                            startActivity(intent);
-                        }
-                    });
-            AlertDialog alert = builder.create();
-            alert.show();
 
         }
     }
 
-    public void changeModeCorrectResult(String wordRu){ //ищу слово и изменяю значение режима для него
+    public void changeModeCorrectResult(String wordRu) { //ищу слово и изменяю значение режима для него
         int c1 = 0; //переменная для считывания текущего значения для каждого слова в режиме 1
         int allModes = 0; //общий показатель слова по всем режимам. Если будет 12, значит слово выучено
         SQLiteDatabase database = dbHelper.getWritableDatabase(); //создаем экз БД
@@ -243,7 +314,7 @@ public class WordTranslateActivity extends AppCompatActivity {
                 null);
 
         if (cursor != null) {
-            if (cursor.moveToFirst()){ // перехожу к 1 возможной строке
+            if (cursor.moveToFirst()) { // перехожу к 1 возможной строке
                 c1 = cursor.getInt(cursor.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_MODE1)) + 1;
                 //считал текущее значение и прибавил к нему 1
                 allModes = c1 + cursor.getInt(cursor.getColumnIndex(TranslateReaderDB.TranslateTexts.COLUMN_MODE2))
@@ -257,7 +328,7 @@ public class WordTranslateActivity extends AppCompatActivity {
         cv.put(TranslateReaderDB.TranslateTexts.COLUMN_MODE1, c1); //закидываю туда новое значение для моего режима
 
         database.update(TranslateReaderDB.TranslateTexts.TABLE_NEW_WORD_NAME, //запрос на изменение определенной строки
-               cv, TranslateReaderDB.TranslateTexts.COLUMN_WORD_RU + " = '" + wordRu + "' ", null);
+                cv, TranslateReaderDB.TranslateTexts.COLUMN_WORD_RU + " = '" + wordRu + "' ", null);
 
         if (allModes >= 9) { //если 12, значит слово изучено
             ContentValues contentValues = new ContentValues(); //создаю строку с этим словом в новой таблице для изученных слов
@@ -270,7 +341,7 @@ public class WordTranslateActivity extends AppCompatActivity {
             contentValues.put(TranslateReaderDB.LearnedWords.COLUMN_DATE, System.currentTimeMillis());
             //также добавил время, когда слово было добавлено
 
-            database.insert(TranslateReaderDB.LearnedWords.TABLE_LEARNED_WORDS_NAME, null , contentValues);
+            database.insert(TranslateReaderDB.LearnedWords.TABLE_LEARNED_WORDS_NAME, null, contentValues);
             //передал данные в новую таблицу и записал
         }
 
@@ -281,9 +352,10 @@ public class WordTranslateActivity extends AppCompatActivity {
 
     /**
      * то же самое для неправильного ответа. Только тут устанавливаем значение в 0.
+     *
      * @param wordRu
      */
-    public void changeModeWrongResult(String wordRu){
+    public void changeModeWrongResult(String wordRu) {
         int c1 = 0;
         SQLiteDatabase database = dbHelper.getWritableDatabase();
 
@@ -293,7 +365,6 @@ public class WordTranslateActivity extends AppCompatActivity {
 
         database.update(TranslateReaderDB.TranslateTexts.TABLE_NEW_WORD_NAME,
                 cv, TranslateReaderDB.TranslateTexts.COLUMN_WORD_RU + " = '" + wordRu + "' ", null);
-
 
 
         database.close();
